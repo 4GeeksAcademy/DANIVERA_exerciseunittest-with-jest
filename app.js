f29dcd4e3de6ec87eef27f893e8cd59ff029c565
DaniVera-tech <daniss.vera@gmail.com>
2025-03-14 11:46:45 +0000

    mi primer test

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-// Declaramos una función con el nombre exacto "formEuroToDollar"
-const fromEuroToDollar = function(valueInEuro) {
-    // Convertimos el valor a dólares
-    let valueInDollar = valueInEuro * 1.07;
-    // Retornamos el valor en dólares
-    return valueInDollar;
-}
 
-
-// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
-module.exports = { sum, fromEuroToDollar }
+function fromEuroToDollar (montoEURO){
+    let USDmonto = montoEURO * 1.07;
+    return USDmonto;
+  }
+  function fromDollarToYen (montoUSD){
+    let YENmonto = montoUSD * 156.5;
+    return YENmonto;
+  }
+  function fromYenToPound (montoYEN){
+    let POUND = montoYEN * 0.87;
+    return POUND;
+  }
+  
+  module.exports = {fromDollarToYen, fromEuroToDollar, fromYenToPound}
